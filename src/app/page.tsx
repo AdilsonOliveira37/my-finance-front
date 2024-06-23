dcddbb2b44c83f5b94e519d50bdd549f677c7dbc
Adilson Oliveira <adilsonreisoliveira.98@gmail.com>
2024-06-22 22:31:39 -0300

    chore: Update title and description in layout.tsx and add bg color to body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,94 @@
-import Image from "next/image";
+interface Categories {
+  id: string;
+  description: string;
+  type: TransactionTypeEnum;
+  created_at: string;
+  deleted_at: string | null;
+}
 
-export default function Home() {
+interface Transaction {
+  description: string;
+  value: number;
+  date: string;
+  transactionType: Categories;
+  id: string;
+  createdAt: string;
+  deletedAt: string | null;
+}
+
+enum TransactionTypeEnum {
+  INCOME = "R",
+  EXPENSE = "D",
+}
+
+const getTransactions = async () => {
+  const response = await fetch("http://localhost:3000/transaction");
+
+  if (!response.ok) {
+    console.error(response.statusText);
+    console.error(response.status);
+    throw new Error("Something went wrong");
+  }
+
+  const data: Transaction[] = await response.json();
+  return data;
+};
+
+export default async function Home() {
+  const transactions = await getTransactions()
+    .then((data) => {
+      console.log(data);
+      const formattedTransactions = data.map((transaction) => {
+        return {
+          ...transaction,
+          date: new Date(transaction.createdAt).toLocaleDateString("pt-BR"),
+          value: new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(transaction.value),
+        };
+      });
+
+      return formattedTransactions;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  const types = {
+    [TransactionTypeEnum.INCOME]: "RECEITA",
+    [TransactionTypeEnum.EXPENSE]: "DESPESA",
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="w-full max-w-7xl flex flex-col items-center ">
+      <header className="w-full flex items-start justify-between my-14 ">
+        <h1 className="text-4xl inline">My Finance Web</h1>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Nova Transação
+        </button>
+      </header>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="w-full">
+        <ol>
+          <li className="w-full grid grid-cols-5 border-b border-gray-500 p-2">
+            <span>Descrição</span>
+            <span>Valor</span>
+            <span>Data</span>
+            <span>Categoria</span>
+            <span>Tipo</span>
+          </li>
+          {transactions?.map((transaction) => (
+            <li key={transaction.id} className="w-full grid grid-cols-5 p-2">
+              <span>{transaction.description}</span>
+              <span>{transaction.value}</span>
+              <span>{transaction.date}</span>
+              <span>{transaction.transactionType?.description}</span>
+              <span>{types[transaction.transactionType?.type]}</span>
+            </li>
+          ))}
+        </ol>
+      </section>
     </main>
   );
 }
